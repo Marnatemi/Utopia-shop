0f7d584370f4996a79affef0338a895046be4c05
Marlena Czerwińska <marlena.stef@gmail.com>
2019-11-12 22:13:03 +0100

    Add missing routing and filters for trips

---
+++ src/redux/tripsRedux.js
@@ -0,0 +1,70 @@
+/* SELECTORS */
+
+export const getAllTrips = ({trips}) => trips;
+
+export const getFilteredTrips = ({trips, filters}) => {
+  let output = trips;
+
+  // filter by search phrase
+  if(filters.searchPhrase){
+    const pattern = new RegExp(filters.searchPhrase, 'i');
+    output = output.filter(trip => pattern.test(trip.name));
+  }
+
+  // TO DO - filter by duration
+  if(filters.duration) {
+    console.log(filters.duration.from, filters.duration.to,);
+    output = output.filter(trip => filters.duration.from <= trip.days && trip.days <= filters.duration.to) ;
+  }
+
+  // DONE - filter by tags
+
+  if(filters.tags){
+    for (let tag of filters.tags) {
+      output =  output.filter(trip => trip.tags.indexOf(tag) >= 0);
+    }
+  }
+
+  // TODO - sort by cost descending (most expensive goes first)
+
+  return output;
+};
+
+export const getTripById = ({trips}, tripId) => {
+  
+  
+  const filtered =  trips.filter(trip => trip.id == tripId);
+
+  console.log('filtering trips by tripId:', tripId, filtered);
+  return filtered.length ? filtered[0] : {error: true};
+};
+
+export const getTripsForCountry = ({trips}, countryCode) => {
+
+  const filtered = trips.filter(trip => trip.country.code == countryCode);
+
+  console.log('filtering trips by countryCode:', countryCode, filtered);
+  return filtered.length ? filtered : [{error: true}];
+};
+
+/* ACTIONS */
+
+/*
+// action name creator
+const reducerName = 'trips';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+// action types
+
+
+// action creators
+
+
+// reducer
+export default function reducer(statePart = [], action = {}) {
+  switch (action.type) {
+    default:
+      return statePart;
+  }
+}
+ */
